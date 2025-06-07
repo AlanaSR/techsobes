b3b766359306dad05a3378249a4562931e461f7c
AlanaSR <svetlanka.shkola@gmail.com>
2025-06-08 03:00:00 +0500

    ex.3 ticket 14

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] %2 ==0){
+                arr[i]+=1;
+            }
+        }
+        System.out.println(Arrays.toString(Arrays.copyOfRange(arr, 2, 7)));
     }
 }
